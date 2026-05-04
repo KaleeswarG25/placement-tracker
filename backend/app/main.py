1f87ef6f255071612ee9a299fbd6be586cf3fdaa
KaleeswarG25 <eswarjass1@gmail.com>
2026-05-04 14:31:50 +0530

    Connect FastAPI backend to PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from .database import engine, Base
+from . import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Student Placement Tracker API")
 
 @app.get("/")
 def home():
     return {
-        "message": "Student Placement Tracker Backend is running"
+        "message": "Student Placement Tracker Backend is running",
+        "database": "Connected successfully"
     }
