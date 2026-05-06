e4c6653e1c3c231c3645cd250311fe33e50f005e
KaleeswarG25 <eswarjass1@gmail.com>
2026-05-06 14:27:32 +0530

    cORS Fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,8 +23,8 @@ allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173,http://127
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
